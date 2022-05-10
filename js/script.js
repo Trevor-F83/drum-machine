0deb0038e632480f0209707e32d2edbd4075a06b
Trevor Faivre <trevorfaivre@hotmail.com>
2022-05-10 18:12:06 -0500

    sounds added to html, functional to keys with js. No css

---
+++ js/script.js
@@ -0,0 +1,79 @@
+document.addEventListener('keydown', function(e) {
+    var myAudio = new Audio('./audio/snare.wav');
+   
+    if (e.key == 'q') {
+        myAudio.play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    var myAudio = new Audio('./audio/closed_high_hat.wav');
+
+    if (e.key == 'w') {
+        myAudio.play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    var myAudio = new Audio('./audio/cowbell.wav');
+
+    if (e.key == 'e') {
+        myAudio.play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    var myAudio = new Audio('./audio/crash.wav');
+
+    if (e.key == 'r') {
+        myAudio.play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    var myAudio = new Audio('./audio/floor_tom.wav');
+
+    if (e.key == 't') {
+    myAudio.play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    var myAudio = new Audio('./audio/kick.wav');
+
+    if (e.key == 'y') {
+        myAudio.play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+        var myAudio = new Audio('./audio/low_tom.wav');
+
+        if (e.key == 'u') {
+            myAudio.play();
+        }
+});
+
+document.addEventListener('keydown', function (e) {
+    var myAudio = new Audio('./audio/open_high _hat.wav');
+
+    if (e.key == 'i') {
+    myAudio.play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    var myAudio = new Audio('./audio/ride.wav');
+
+    if(e.key == 'o') {
+        myAudio.play();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    var myAudio = new Audio('./audio/tom_high.wav');
+
+    if (e.key == 'p') {
+        myAudio.play();
+    }
+});
