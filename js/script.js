5cb2ebd7b9dc85a8c48a9d922e5cd495bd2ae9a6
Trevor Faivre <trevorfaivre@hotmail.com>
2022-05-11 14:12:55 -0500

    recieved access key form unsplash. Having trouble linking unsplash to project using AJAX. Some api  code left over that didnt work.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,43 @@
-document.addEventListener('keydown', function(e) {
-    var myAudio = new Audio('./audio/snare.wav');
-   
+// const URL = 'https://api.unsplash.com/photos/?client_id=AsYq44X9bUjvLXG4Um1uB53gCwIFh_KKOoGxPDr_ojs=s/photos/music'
+
+// const numItemsToGenerate = 1;
+
+// function renderItem() {
+//     fetch('https://source.unsplash.com/s/photos/music').then((response) => {
+//         let item = document.createElement('div');
+//         item.classList.add('item');
+//         item.innerHTML = `<img class='music-image' src='${data.img}'/>`
+//         document.body.appendChild(item);
+//     })
+// }
+
+
+//from the lesson day 2 AJAX
+function handleGetDate(){
+
+    $.ajax(URL).then(function(data) {
+        console.log('img data is ready')
+        $data.img(data.img)
+        $('main').append(`<img src='${data.img}'/>`)
+        console.log(error)
+    })
+}
+
+
+
+
+
+//defines keydown event
+document.addEventListener('keydown', function (e) {
+    var myAudio = new Audio('./audio/snare.wav'); //tells the function where to get info once keydown event is initiated
+
+    //is function in action. Repeat from "document." to "});" for each sound
     if (e.key == 'q') {
         myAudio.play();
     }
 });
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
     var myAudio = new Audio('./audio/closed_high_hat.wav');
 
     if (e.key == 'w') {
@@ -14,7 +45,7 @@ document.addEventListener('keydown', function(e) {
     }
 });
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
     var myAudio = new Audio('./audio/cowbell.wav');
 
     if (e.key == 'e') {
@@ -22,7 +53,7 @@ document.addEventListener('keydown', function(e) {
     }
 });
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
     var myAudio = new Audio('./audio/crash.wav');
 
     if (e.key == 'r') {
@@ -30,15 +61,15 @@ document.addEventListener('keydown', function(e) {
     }
 });
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
     var myAudio = new Audio('./audio/floor_tom.wav');
 
     if (e.key == 't') {
-    myAudio.play();
+        myAudio.play();
     }
 });
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
     var myAudio = new Audio('./audio/kick.wav');
 
     if (e.key == 'y') {
@@ -46,31 +77,31 @@ document.addEventListener('keydown', function(e) {
     }
 });
 
-document.addEventListener('keydown', function(e) {
-        var myAudio = new Audio('./audio/low_tom.wav');
+document.addEventListener('keydown', function (e) {
+    var myAudio = new Audio('./audio/low_tom.wav');
 
-        if (e.key == 'u') {
-            myAudio.play();
-        }
+    if (e.key == 'u') {
+        myAudio.play();
+    }
 });
 
 document.addEventListener('keydown', function (e) {
     var myAudio = new Audio('./audio/open_high _hat.wav');
 
     if (e.key == 'i') {
-    myAudio.play();
-    }
-});
-
-document.addEventListener('keydown', function(e) {
-    var myAudio = new Audio('./audio/ride.wav');
-
-    if(e.key == 'o') {
         myAudio.play();
     }
 });
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
+    var myAudio = new Audio('./audio/ride.wav');
+
+    if (e.key == 'o') {
+        myAudio.play();
+    }
+});
+
+document.addEventListener('keydown', function (e) {
     var myAudio = new Audio('./audio/tom_high.wav');
 
     if (e.key == 'p') {
